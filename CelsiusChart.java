4bf9efc93dc161802a2bc939c193bb380c139499
Joseph Masterson <43124604+JMasterson513@users.noreply.github.com>
2018-10-30 16:15:27 -0400

    Add files via upload

---
+++ CelsiusChart.java
@@ -0,0 +1,16 @@
+//Joseph Masterson
+
+public class CelsiusChart {
+
+	public static void main(String[] args) {
+		
+		double C = 0;
+
+		for(double i = C; i <= 20; i++)
+		{
+			double F = ((9 * i) / 5 + 32);
+			System.out.println(i + "°C		" + F + "° F");
+		}
+	}
+
+}
